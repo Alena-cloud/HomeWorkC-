a48a7f9765d87c63ccaaa290c3d89e7c343f3ef0
alena <alena_sc@outlook.com>
2022-12-02 14:20:40 -0600

    added some info to file Tsak7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -4,19 +4,16 @@
 //1 -> нет
 
 Console.WriteLine("Enter number indicating the day of the week: ");
-int dayNumber = Convert.ToInt32(Console.ReadLine());
+int dayNumber = int.Parse(Console.ReadLine()!);
 
-void VerifyingTheDayOfTheWeek(int dayNumber)
 {
     if (dayNumber == 6 || dayNumber == 7)
    {
-    Console.WriteLine("(This day is weekend) - > Yes");
+    Console.WriteLine("This day is weekend");
 }
    else if (dayNumber < 1 || dayNumber > 7)
    {
     Console.WriteLine("This is not a day of the week");
 }
-else Console.WriteLine("(This day is not weekend) - > No");
+else Console.WriteLine("This day is not weekend");
 }
-
-VerifyingTheDayOfTheWeek(dayNumber);
